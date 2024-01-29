c481c6b82ae4d0d5715fade564df9a067f0c12e5
SanjhanaB <146237504+SanjhanaB@users.noreply.github.com>
2024-01-29 21:34:59 +0100

    push

--- analysis.py
+++ analysis.py
@@ -1,19 +1,19 @@
-def count_unbound_water(grid, total_height, total_width, cell_type=0):
-    unbound_water = 0
+def count_unbound_molecule(grid, total_height, total_width, cell_type=1):
+    unbound_molecule = 0
 
-    for height in total_height:
-        for width in total_width:
+    for height in range(total_height):
+        for width in range(total_width):
             neighbours = get_neighbours(height, width, grid, total_height, total_width)
 
             if grid[height, width] == 0 or grid[height, width] == cell_type:
-                    continue
+                continue
 
-            if any([i[2] != 0 for i in neighbours]):
+            if any([i[2] == 2 for i in neighbours]):
                 continue
             else:
-                unbound_water += 1
+                unbound_molecule += 1
                 
-    return unbound_water
+    return unbound_molecule
 
 
 def get_neighbours(height, width, grid, total_height, total_width):
@@ -26,4 +26,6 @@ def get_neighbours(height, width, grid, total_height, total_width):
     neighbours.append((height, ((width + 1) % total_width), grid[height, ((width + 1) % total_width)]))
 
     return neighbours
+
+
 
